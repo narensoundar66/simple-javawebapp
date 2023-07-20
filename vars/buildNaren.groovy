b73d5badddf2dcb48e7ebd7062d41a9d5cd73e7f
naren <narenlalitha@gmail.com>
2023-07-20 12:24:46 -0400

    added changes to buildNaren &jenkinsfile

--- vars/buildNaren.groovy
+++ vars/buildNaren.groovy
@@ -1,5 +1,10 @@
 def call(var1, var2)
 {
- echo "${var1}"
- echo "${var2}"
+ int var1int = var1.toInteger()
+ int var2int = var2.toInteger()
+
+ int result = var1int + var2int
+
+ echo "${result}"
+
 }
