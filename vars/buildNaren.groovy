7baa79c541aa1d3973b32d99f17894e4828857fc
naren <narenlalitha@gmail.com>
2023-07-20 23:11:00 -0400

    added changes to buildNaren &jenkinsfile

--- vars/buildNaren.groovy
+++ vars/buildNaren.groovy
@@ -7,7 +7,7 @@ def call(input)
  else if ("${input}" == "add")    //adding numbers if add is input
  {
   echo "doing addition"
-  int x = input as int  //converting input to integer for adding
+  int x = 6 //converting input to integer for adding
   int y = 6
   int ans = x + y      // can we use 6 explicitly
   echo "${ans}"
